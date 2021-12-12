b02bbe590ddbcf389ba9d3f80b604d68d9765842
N-Kairi <31227478+kairi812@users.noreply.github.com>
2021-12-12 09:51:44 +0900

    [change] リアルタイム処理への変更
    画像からではなく、リアルタイムでカメラで読み取っての処理に変更した

--- src/face_recognition.py
+++ src/face_recognition.py
@@ -1,30 +1,41 @@
 '''
 Create date : 2021/12/10.
-Update      : 2021/12/11.
+Update      : 2021/12/12.
 Writer      : Nishimura Kairi
 '''
 
 import cv2
-import matplotlib.pyplot as plt
-
-def main():
-    CASCADE_PATH = 'C:/opencv/sources/data/haarcascades/'\
-                    'haarcascade_frontalface_default.xml'
-    INPUT_IMG_PATH = "src/data/img/lena.png"
-    OUTPUT_IMG_PATH = "src/data/output/face_recognition.png"
-
-    classifier = cv2.CascadeClassifier(CASCADE_PATH)
-
-    img = cv2.imread(INPUT_IMG_PATH)
-
-    color = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
-    targets = classifier.detectMultiScale(color)
-
-    for x,y,w,h in targets:
-        cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
-
-    cv2.imwrite(OUTPUT_IMG_PATH, img)
+import matplotlib.pyplot as plt    
 
 if __name__ == '__main__':
-    main()
+    # constants define
+    DEVICE_ID = 0
+    # path reading
+    CASCADE_PATH = 'C:/opencv/sources/data/haarcascades/'\
+                    'haarcascade_frontalface_alt2.xml'
+    # classifier define
+    classifier = cv2.CascadeClassifier(CASCADE_PATH)
+    # video capture getting
+    cap = cv2.VideoCapture(DEVICE_ID)
+    end_flag, frame = cap.read()
+    h, w = frame.shape[:2]
+    # real-time proccesing
+    while end_flag == True:
+        targets = classifier.detectMultiScale(frame, minSize=(100,100))
+        # target drowing
+        for x, y, w, h in targets:
+            cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
+        # display
+        cv2.imshow("org", frame)
+
+        # if put on Esc key -> end proccesing
+        key = cv2.waitKey(33)
+        if key == 27:
+            break
+
+        # update
+        end_flag , frame = cap.read()
+    
+    # end proccesing
+    cv2.destroyAllWindows()
+    cap.release()
