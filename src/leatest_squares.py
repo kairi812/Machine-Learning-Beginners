14cc2afc3a47d74ea15faabb7b9a342133d446cd
N-Kairi <31227478+kairi812@users.noreply.github.com>
2021-12-15 13:11:35 +0900

    [change] 最小二乗法プログラムの変更
    上手く動作しない部分を修正。答えが合っているかを確かめておく。

--- src/leatest_squares.py
+++ src/leatest_squares.py
@@ -3,40 +3,44 @@ import numpy as np
 def main():
     x, y = [], []
     
-    n = 2 # データ数
+    n = 5 # データ数
     print("フィッティングさせる方程式の次元数を入力してください")
     k = int(input()) # 次元数
 
-    S = np.zeros((k+1, k+1))
+    S = np.zeros((k+1, k+2))
     T = np.zeros((k+1, 1))
 
     print("データを入力してください")
     for i in range(n):
+        print("x_{}=".format(i), end="")
         x.append(float(input()))
+        print("y_{}=".format(i), end="")
         y.append(float(input()))
     
     for i in range(k+1):
         for j in range(k+1):
             S[i][j] = sum([xs**(i+j) for xs in x])
-    
-    for i in range(k+1):
-        T[i][0] = sum([yt*(xt**i) for xt in x for yt in y])
+        S[i][k+1] = sum([yt*(xt**i) for xt in x for yt in y])
+    # for i in range(k+1):
+    #     T[i][0] = sum([yt*(xt**i) for xt in x for yt in y])
+    print(S)
 
-    a = gauss_j(S, T, k)
+    a = gauss_j(S, k)
     
-def gauss_j(S, T, k):
-    for k in range(k):
-        pivot = S[k][k]
+def gauss_j(S, k):
+    for n in range(k+1):
+        pivot = S[n][n]
+        for i in range(k+2):
+            S[n][i] /= pivot
+
         for i in range(k+1):
-            S[k][i] = S[k][i] / pivot
+            if i != n:
+                d = S[i][n]
+                for j in range(k+2):
+                    S[i][j] -= d * S[n][j]
 
-        for i in range(k):
-            if i != k:
-                d = S[i][k]
-                for j in range(k+1):
-                    S[i][j] = S[i][j] - d * S[k][j]
-
-    for i in range(k):
-        print(S[i][k])
+    print(S)
+    for i in range(k+1):
+        print(S[i][k+1])
 
 main()
